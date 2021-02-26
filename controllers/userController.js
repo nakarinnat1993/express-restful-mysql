ab6213db655b4aa533326671c98d360939c17481
unknown <nakarinnat1993@gmail.com>
2021-02-26 12:12:28 +0700

    connect db by orm

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,27 @@
-exports.index = (req, res, next) => {
+const models = require('../models/index');
+
+exports.index = async (req, res, next) => {
+
+    // const users = await models.User.findAll();
+    // const users = await models.User.findAll({
+    //     // attributes:['id','name','email'],
+    //     attributes:{exclude:['password']},
+    //     // where:{
+    //     //     email:'nut@mail.com'
+    //     // },
+    //     order:[['id','desc']]
+    // });
+    const users = await models.User.findAll({
+        attributes:['id','name',['email','username']], // as 
+        order:[['id','desc']]
+    });
+    // const sql = 'select id,name from users';
+    // const users = await models.sequelize.query(sql,{
+    //     type: models.sequelize.QueryTypes.SELECT
+    // })
+
+
     res.status(200).json({
-        message: 'แสดงรายการ Users'
+        data: users
     });
 }
