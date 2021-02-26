ad72aa1fdc069c232930968697cfc20a5ca78fad
unknown <nakarinnat1993@gmail.com>
2021-02-26 22:09:15 +0700

    env

---
+++ config/config.js
@@ -0,0 +1,25 @@
+const config = require('./index');
+
+module.exports = {
+    "development": {
+        "username": config.DB_USERNAME,
+        "password": config.DB_PASSWORD,
+        "database": config.DB_DATABASE,
+        "host": config.DB_HOST,
+        "dialect": config.DB_DIALECT
+      },
+      "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+      },
+      "production": {
+        "username": config.DB_USERNAME_PROD,
+        "password": config.DB_PASSWORD_PROD,
+        "database": config.DB_DATABASE_PROD,
+        "host": config.DB_HOST_PROD,
+        "dialect": config.DB_DIALECT_PROD
+      }
+}
