fe2f924af333e7e944d41d4679fc1fa3cd905e5a
unknown <nakarinnat1993@gmail.com>
2021-02-27 10:57:06 +0700

    Basic Auth

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const basicAuth = require('express-basic-auth')
+
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const blogsRouter = require('./routes/blogs');
 
 const app = express();
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/', indexRouter);
 app.use('/api/users', usersRouter);
-app.use('/api/blogs', blogsRouter);
+app.use('/api/blogs', basicAuth({
+    users: { 'admin': '1234' }
+}), blogsRouter);
 
 module.exports = app;
