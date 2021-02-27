2e62a0d06903c28401c72ce90ef2e251a6426eb3
unknown <nakarinnat1993@gmail.com>
2021-02-27 10:57:40 +0700

    Helmet protect header

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const basicAuth = require('express-basic-auth')
-
+const basicAuth = require('express-basic-auth');
+const helmet = require("helmet");
 
 
 const indexRouter = require('./routes/index');
@@ -13,7 +13,7 @@ const blogsRouter = require('./routes/blogs');
 const app = express();
 
 
-
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
